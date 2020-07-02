c42a7fc05a632bff91411827173e35c8ec723b03
Andrey Lozovoy <lozovoya@gmail.com>
2020-07-02 16:27:36 +0300

    mcc code search modified

--- pkg/card/mcc.go
+++ pkg/card/mcc.go
@@ -2,20 +2,16 @@ package card
 
 func TranslateMCC(code string) string {
 
+	const notDefined = "Значение не определено"
 	mcc := map[string]string{
 		"5411": "Супермаркеты",
 		"5812": "Рестораны",
 		"8765": "Крокодильни",
 	}
-	description := ""
 
-	for key, value := range mcc {
-		if key == code {
-			description = value
-		}
+	if value, ok := mcc[code]; ok {
+		return value
+	} else {
+		return notDefined
 	}
-	if description == "" {
-		description = "Категория не указана"
-	}
-	return description
 }
